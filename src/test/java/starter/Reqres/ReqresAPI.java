104bd9d13915ac6050c16760a5d153f24910b4f3
egarifs <egarifs25@gmail.com>
2023-05-17 18:26:11 +0700

    Fix path on ReqresAPI

--- src/test/java/starter/Reqres/ReqresAPI.java
+++ src/test/java/starter/Reqres/ReqresAPI.java
@@ -8,24 +8,21 @@ import java.io.File;
 
 public class ReqresAPI {
     public static String BASE_URL = "https://reqres.in";
-
     public static final String DIR = System.getProperty("user.dir");
-
-    public static String REQ_BODY_DIR = DIR+"src/test/resources/features/JSON/ReqBody";
-
+    public static String REQ_BODY_DIR = DIR+"/src/test/resources/JSON/ReqBody/";
+    public static String JSON_SCHEMA_DIR = DIR+"/src/test/resources/JSON/JSONSchema/";
     public static String GET_LIST_USERS = BASE_URL+"/api/users?page={page}";
-
     public static String POST_CREATE_USER = BASE_URL+"/api/users";
 
     @Step("Get list user with parameter page")
     public void getListUserValidParamPage(int page){
-        SerenityRest.given().pathParams("page",page);
+        SerenityRest.given()
+        .pathParams("page",page);
     }
-
     @Step("Post Create new user")
     public void postCreateUser(File json){
         SerenityRest.given()
-                .contentType(ContentType.JSON).body(json)
-                .body(json);
+        .contentType(ContentType.JSON)
+        .body(json);
     }
 }
